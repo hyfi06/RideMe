b583a556228c9e1a7aaa2417d6adcdd2fed275f7
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2019-10-12 11:52:26 -0500

    [ADD] UserProfile

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from '../components/Login';
+import UserProfile from '../components/UserProfile';
 import '../assets/styles/App.scss';
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path='/login' component={Login} />
     <Route exact path='/' component={Login} />
+    <Route exact path='/login' component={Login} />
+    <Route exact path='/profile' component={UserProfile} />
   </BrowserRouter>
 );
 
