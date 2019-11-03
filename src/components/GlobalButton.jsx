d7e347beec3a8ce2339c7ea12f41f2dd247cc393
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2019-11-03 13:16:45 -0600

    [ADD] Home

---
+++ src/components/GlobalButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/components/button.scss';
+
+const GlobalButton = ({children, typeButton }) => (
+  <div className='button-container'>
+    <button className='button' type={typeButton}> {children} </button>
+  </div>
+);
+
+export default GlobalButton;
