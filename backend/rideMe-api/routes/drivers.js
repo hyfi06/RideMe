a9fb976a01a539f6d44096b7ebeca4a948806f62
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2019-11-10 16:45:23 -0600

    [ADD] MongoDB CRUD

--- backend/rideMe-api/routes/drivers.js
+++ backend/rideMe-api/routes/drivers.js
@@ -21,20 +21,99 @@ function driversApi(app) {
     }
   });
 
+  router.get('/id/:driverId', async (req, res, next) => {
+    const { driverId } = req.params;
+
+    try {
+      const driver = await driversService.getDriver({ driverId });
+
+      res.status(200).json({
+        data: driver,
+        message: 'driver retrieved',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
   router.get('/near', async (req, res, next) => {
-    const { lat, lng } = req.query;
+    const { lat, lng, range } = req.query;
+
     if (!lat || !lng) {
-      res.status(500).json({
+      res.status(400).json({
         message: 'lat and lng require',
       });
+    } else {
+      const drivers = await driversService.getNearbyDrivers({ lat, lng }, range);
+      res.status(200).json({
+        data: drivers,
+        message: 'nearbly drivers listed',
+      });
     }
-    const drivers = await driversService.getNearbyDrivers({ lat, lng }, 0.01);
-
-    res.status(200).json({
-      data: drivers,
-      message: 'drivers nearbly listed',
-    });
   });
+  
+  router.get('/nearMock', async (req, res, next) => {
+    const { lat, lng, range } = req.query;
+
+    if (!lat || !lng) {
+      res.status(400).json({
+        message: 'lat and lng require',
+      });
+    } else {
+      const drivers = await driversService.getNearbyDriversMock({ lat, lng }, range);
+      res.status(200).json({
+        data: drivers,
+        message: 'nearbly drivers listed',
+      });
+    }
+  });
+
+  router.post('/', async (req, res, next) => {
+    const { body: driver } = req;
+
+    try {
+      const createdDriverId = await driversService.createDriver({ driver });
+
+      res.status(200).json({
+        data: createdDriverId,
+        message: 'driver created',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/id/:driverId', async (req, res, next) => {
+    const { driverId } = req.params;
+
+    try {
+      const deletedDriverId = await driversService.deleteDriver({ driverId });
+
+      res.status(200).json({
+        data: deletedDriverId,
+        message: 'driver deleted',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.patch('/id/:driverId', async (req, res, next) => {
+    const { driverId } = req.params;
+    const { body: driver } = req;
+
+    try {
+      const updatedDriverId = await driversService.updateDriver({ driverId, driver });
+
+      res.status(200).json({
+        data: updatedDriverId,
+        message: 'driver updated',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
 };
 
 module.exports = driversApi;
--- backend/rideMe-api/services/drivers.js
+++ backend/rideMe-api/services/drivers.js
@@ -1,19 +1,19 @@
-// const MongoLib = require('../lib/mongo');
+const MongoLib = require('../lib/mongo');
 const { driversMock } = require('../utils/MOCK_DRIVERS_v2');
 
 class DriversServices {
-  // constructor() {
-  //   this.collection = 'drivers';
-  //   this.mongoDB = new MongoLib();
-  // }
+  constructor() {
+    this.collection = 'drivers';
+    this.mongoDB = new MongoLib();
+  }
 
-  async getDrivers() {
+  async getDriversMock() {
     this.mongo = false;
     const drivers = await Promise.resolve(driversMock);
     return drivers || [];
   };
 
-  async getNearbyDrivers({ lat, lng }, resolution = 0.01) {
+  async getNearbyDriversMock({ lat, lng }, resolution = 0.01) {
     this.mongo = false;
     const drivers = await Promise.resolve(driversMock);
     return drivers.filter((driver) => {
@@ -26,24 +26,47 @@ class DriversServices {
     });
   };
 
-  // async getDriversMongo({ lat, lng }) {
-  //   const query = {
-  //     coord.lat: {
-  //       $gte: parseFloat(lat) - resolution,
-  //       $lte: parseFloat(lat) + resolution,
-  //     },
-  //     coord.lng: {
-  //       $gte: parseFloat(lng) - resolution,
-  //       $lte: parseFloat(lng) + resolution,
-  //     },
-  //   };
-  //   const drivers = await this.mongoDB.getAll(this.collection, query);
-  //   return drivers || [];
-  // };
+  async getDrivers() {
+    const drivers = await this.mongoDB.getAll(this.collection);
+    return drivers || [];
+  };
 
-  // async createDriver ({ Driver }) {
-  //   const createdDriverId = await this.mongoDB
-  // }
+  async getDriver({ driverId }) {
+    const driver = await this.mongoDB.get(this.collection, driverId);
+    return driver || {};
+  };
+
+  async getNearbyDrivers({ lat, lng }, range = 0.01) {
+    const query = {
+      'coord.lat': {
+        $gte: parseFloat(lat) - range,
+        $lte: parseFloat(lat) + range,
+      },
+      "coord.lng": {
+        $gte: parseFloat(lng) - range,
+        $lte: parseFloat(lng) + range,
+      },
+    };
+
+    const drivers = await this.mongoDB.getAll(this.collection, query);
+
+    return drivers || [];
+  };
+
+  async createDriver({ driver }) {
+    const createdDriverId = await this.mongoDB.create(this.collection, driver);
+    return createdDriverId;
+  }
+
+  async updateDriver({ driverId, driver } = {}) {
+    const updatedDriverId = await this.mongoDB.update(this.collection, driverId, driver);
+    return updatedDriverId;
+  }
+
+  async deleteDriver({ driverId }) {
+    const deletedDriverId = await this.mongoDB.delete(this.collection, driverId);
+    return deletedDriverId;
+  };
 }
 
 module.exports = DriversServices;
